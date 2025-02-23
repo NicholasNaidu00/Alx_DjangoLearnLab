5430e5e8748a6a86517450953794fb4e6e6680ff
Nicholas Naidu <nicholasnaidu@ymail.com>
2025-02-23 20:40:19 +0200

    10

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,11 @@ from .models import Book, Library  # Importing both Book and Library models
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view to list all books
@@ -72,3 +77,15 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import ListView, DetailView
+from .models import Book, Library  # Importing both Book and Library models
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books
